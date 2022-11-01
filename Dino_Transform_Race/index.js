34471630daa279957c3618122131dbc9c5eb1fc7
danding <dandingkeji.top@gmail.com>
2022-11-01 22:46:56 +0800

    #Dino_Transform_Race

---
+++ Dino_Transform_Race/index.js
@@ -0,0 +1,3 @@
+window.screenOrientation = "sensor_landscape", loadLib("libs/min/laya.core.min.js"), 
+loadLib("libs/min/laya.ani.min.js"), loadLib("libs/min/laya.ui.min.js"), loadLib("libs/min/laya.d3.min.js"), 
+loadLib("libs/min/laya.physics3D.min.js"), loadLib("js/bundle.js");
