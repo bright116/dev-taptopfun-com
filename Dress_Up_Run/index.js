61bc92cf80b58d8709431abac4532afaef316185
danding <dandingkeji.top@gmail.com>
2022-11-03 19:53:35 +0800

    #Dress_Up_Run

---
+++ Dress_Up_Run/index.js
@@ -0,0 +1,23 @@
+//-----libs-begin-----
+
+//-----libs-end-------
+/*
+ * @Author: your name
+ * @Date: 2022-01-10 11:31:21
+ * @LastEditTime: 2022-01-16 09:33:58
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \Empty Progectse:\Laya\Dress Up Run\bin\index.js
+ */
+
+loadLib("libs/laya.core.js"),
+loadLib("libs/laya.ani.js"),
+loadLib("libs/laya.ui.js"),
+loadLib("libs/laya.tiledmap.js"),
+loadLib("libs/laya.d3.js"),
+loadLib("libs/laya.html.js"),
+loadLib("libs/laya.particle.js"),
+// loadLib("libs/laya.physics_o.js")
+
+loadLib("libs/min/laya.physics3D.min.js");
+loadLib("js/bundle.js");
